ceea034975d91a6a2a4680f5f5116d38ec9d26a5
Yura Trotskyi <>
2018-10-04 19:56:08 +0300

    added reducers index and firebase

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase'
+import reducers from './reducers'
+
+const store = createStore(reducers);
 
 class App extends Component {
+	componetWillMount() {
+		const config = {
+    		apiKey: 'AIzaSyA1dr9Nl5rqHMsfR32Hcvza1rLI3plT_nA',
+   			authDomain: 'manager-693c6.firebaseapp.com',
+    		databaseURL: 'https://manager-693c6.firebaseio.com',
+    		projectId: 'manager-693c6',
+    		storageBucket: 'manager-693c6.appspot.com',
+    		messagingSenderId: '806765340083'
+  		};
+  		firebase.initializeApp(config);
+	}
+
 	render() {
 		return (
-			// <Provider store={store}>
+			<Provider store={store}>
 				<View>
 					<Text>
 						Hello world!
 					</Text>
 				</View>
-			// </Provider>
-		)
+			</Provider>
+		);
 	}
 }
 
